12c4a0da9a6ebbfaae1ba0ed1867936799e84d60
PaulGoncalves <paul.goncalves@ynov.com>
2016-04-13 11:25:56 +0200

    moncompte

---
+++ Site de vente/moncompte.php
@@ -0,0 +1,42 @@
+<?php include("assets/header4.php");?>  
+<div class="main">
+    <div class="content">
+        <div class="content_top">
+            <div class="heading">
+                <h3>MON COMPTE</h3>
+            </div>
+            <div class="see">
+
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="section group">
+            <div class="col span_1_of_3">
+                <img src="web/images/image-de-profil-2.jpg" width=70% height=50%/><br><br><br>
+                <a href="#" class="myButton">Modifié</a>
+            </div>
+            <div class="col span_1_of_3">
+                <br><h3>Pseudo</h3><br>
+                <br><h3>Prenom</h3><br><br>
+                <h3>Nom</h3><br><br>
+                <h3>Mail</h3><br><br>
+            </div>
+            <div class="col span_1_of_3">
+                <br><br><a href="#" style="text-decoration:none;">Mes achats</a><br><br><br><br><br><br>
+                <a href="#" style="text-decoration:none;">Mot passe oublié</a><br><br>
+            </div>
+        </div>
+    </div>
+</div>           
+
+<?php include("assets/footer.php");?>
+
+<script type="text/javascript">
+    $(document).ready(function() {			
+        $().UItoTop({ easingType: 'easeOutQuart' });
+
+    });
+</script>
+<a href="#" id="toTop"><span id="toTopHover"> </span></a>
+</body>
+</html>
