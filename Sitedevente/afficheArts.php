826ddd827a183d41c19a582c805ad4387285ba5d
blackmister <redmister94@gmail.com>
2016-04-20 15:35:31 +0200

    objet

---
+++ Sitedevente/afficheArts.php
@@ -0,0 +1,71 @@
+<?php include("../assets/header3.php"); ?>
+<div class="header_slide">
+    <div class="header_bottom_left">				
+        <div class="categories">
+            <ul>
+                <h3>Catégories</h3>
+                <li><a href="categorie/categorie.php">Multimédia</a></li>
+                <li><a href="categorie/categorie2.php">Fournitures</a></li>
+                <li><a href="categorie/categorie3.php">Meubles</a></li>
+                <li><a href="categorie/categorie4.php">Accessoires</a></li>
+                <li><a href="categorie/categorie5.php">Logiciels</a></li>
+
+            </ul>
+        </div>					
+    </div>
+    <div class="header_bottom_right">					 
+        <!--début classement -->
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th></th><th style="">Nom</th><th>Disponibilité</th><th>Prix</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                <tr>
+                    <td><a href=""><img src="../web/images/art.png" class="col-md-5 col-xs-6" alt=""></a></td>
+                    <td><h4><a href="">Titre article 1 </a><br><small>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur.</small></h4></td>
+                    <td><img src="../web/images/dispo.png" class="col-md-3 col-xs-6" alt=""></td>
+                    <td><h2>22€</h2></td>
+                </tr>
+                <tr>
+                    <td><a href=""><img src="../web/images/art.png" class="col-md-5 col-xs-6" alt=""></a></td>
+                    <td><h4><a href="">Titre article 1 </a><br><small>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur.</small></h4></td>
+                    <td><img src="../web/images/dispo.png" class="col-md-3 col-xs-6" alt=""></td>
+                    <td><h2>30€</h2></td>
+                </tr>
+                <tr>
+                     <td><a href=""><img src="../web/images/art.png" class="col-md-5 col-xs-6" alt=""></a></td>
+                    <td><h4><a href="">Titre article 1 </a><br><small>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur.</small></h4></td>
+                    <td><img src="../web/images/nonDispo.jpg" class="col-md-3 col-xs-6" alt=""></td>
+                    <td><h2>55€</h2></td>
+                </tr>
+                <tr>
+                     <td><a href=""><img src="../web/images/art.png" class="col-md-5 col-xs-6" alt=""></a></td>
+                    <td><h4><a href="">Titre article 1 </a><br><small>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur.</small></h4></td>
+                    <td><img src="../web/images/dispo.png" class="col-md-3 col-xs-6" alt=""></td>
+                    <td><h2>10€</h2></td>
+                </tr>
+                <tr>
+                     <td><a href=""><img src="../web/images/art.png" class="col-md-5 col-xs-6" alt=""></a></td>
+                    <td><h4><a href="">Titre article 1 </a><br><small>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur.</small></h4></td>
+                    <td><img src="../web/images/nonDispo.jpg" class="col-md-3 col-xs-6" alt=""></td>
+                    <td><h2>28€</h2></td>
+                </tr>
+                <tr>
+                     <td><a href=""><img src="../web/images/art.png" class="col-md-5 col-xs-6" alt=""></a></td>
+                    <td><h4><a href="">Titre article 1 </a><br><small>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur.</small></h4></td>
+                    <td><img src="../web/images/nonDispo.jpg" class="col-md-3 col-xs-6" alt=""></td>
+                    <td><h2>20€</h2></td>
+                </tr>
+            </tbody>
+        </table>
+
+
+        <!--fin classement -->
+    </div>
+    <div class="clear"></div>
+</div>
+<!-- footer -->
+<?php include("../assets/footer.php"); ?>
