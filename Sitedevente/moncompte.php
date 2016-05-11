f07907bde268e2710004570f9382d75a3e590fc7
PaulGoncalves <paul.goncalves@ynov.com>
2016-05-11 12:16:12 +0200

    Modification page Moncompte

--- Sitedevente/moncompte.php
+++ Sitedevente/moncompte.php
@@ -1,6 +1,6 @@
-<?php include("assets/header.php");?>  
+<?php include("assets/header4.php");?>  
 <div class="main">
-    <div class="content">
+    <div class="content"><br><br>
         <div class="content_top">
             <div class="heading">
                 <h3>MON COMPTE</h3>
@@ -9,21 +9,47 @@
 
             </div>
             <div class="clear"></div>
-        </div>
+        </div><br><br>
         <div class="section group">
             <div class="col span_1_of_3">
-                <img src="web/images/image-de-profil-2.jpg" width=70% height=50%/><br><br><br>
-                <a href="#" class="myButton">Modifier</a>
+                <img src="web/images/image-de-profil-2.jpg" style="border:1px solid grey;" width=70% height=50%/><br><br><br><br><br>
+                <a href="#" class="myButton" style="margin-top:1%;">Modifiez la photo</a><br><br><br>
+                <br><br><a href="#" id="lien_mes_achats" style="text-decoration:none;"><i class="fa fa-shopping-basket fa-1x" aria-hidden="true"></i>	Mes achats</a>
             </div>
             <div class="col span_1_of_3">
-                <br><h3>Pseudo</h3><br>
-                <br><h3>Prenom</h3><br><br>
-                <h3>Nom</h3><br><br>
-                <h3>Mail</h3><br><br>
+                <div class="contact-form">
+                    <form method="post" action="contact-post.php"><br><br>
+                        <div>
+                            <span><label><h3>Nom</h3></label></span>
+                        </div><br><br><br><br>
+                        <div>
+                            <span><label><h3>Prénom</h3></label></span>
+                        </div><br><br><br><br>                        <div>
+                            <span><label><h3>Mail</h3></label></span>
+                        </div><br><br><br><br>
+                        <a href="#" class="myButton">Modifiez les informations</a>
+                    </form>
+                </div>
             </div>
             <div class="col span_1_of_3">
-                <br><br><a href="#" style="text-decoration:none;">Mes achats</a><br><br><br><br><br><br>
-                <a href="#" style="text-decoration:none;">Mot passe oublié</a><br><br>
+                <div class="contact-form">
+                    <form method="post" action="contact-post.php">
+                        <h3><u>Modifiez mot de passe/Pseudo :</u></h3><br><br>
+                        <div>
+                            <span><label>Taper le nouveau pseudo:</label></span>
+                            <span><input name="pseudo" type="text" class="textbox" ></span>
+                        </div><br><br>
+                        <div>
+                            <span><label>Nouveau mot de passe :</label></span>
+                            <span><input name="password" type="text" class="textbox" ></span>
+                        </div><br><br>
+                        <div>
+                            <span><label>Retapez le nouveau mot de passe :</label></span>
+                            <span><input name="password" type="text" class="textbox" ></span>
+                        </div><br><br><br><br><br><br><br><br>
+                        <a href="#" class="myButton" style="margin-top:2%;">Modifiez le mot de passe</a>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
